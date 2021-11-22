d42a59c29657c1bc8cf7af81fb920e53dc0c1909
Benoit Triay <benoit.tria+13y@gmail.com>
2021-11-22 20:37:09 +0000

    main: update start-up and engine call

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 
-#include <array>
-
 #include "utils\io.h"
+#include "Grid.h"
 
 int main(int argc, char* argv[])
 {
@@ -14,19 +13,19 @@ int main(int argc, char* argv[])
         filename = argv[1];
     }
     else {
-        filename = "../../test/grid.txt";
+        filename = "input_grid.txt";
     }
     
     auto grid_array = io::read_txt_grid(filename);
 
     if (!grid_array.has_value())
-    {
-        std::cout << "no value\n";
         return 1;
-    }
 
-    for (const auto& v : grid_array.value())
-        std::cout << v << ", ";
+    engine::Grid g{ grid_array.value()};
 
+    std::cout << g;
+    g.clean_from_existing_solution();
+    g.check_unique_value();
+    std::cout << g;
 
 }
