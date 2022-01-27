f308dafaab15a4b6255be000755f2ec3473a0bd2
Benoit Triay <benoit.tria+13y@gmail.com>
2022-01-27 15:49:51 +0000

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-import <string>;
+import <fstream>;
 
 import io;
 import Grid;
@@ -6,19 +6,21 @@ import GridEngine;
 
 int main(int argc, char* argv[])
 {
+    auto input_filename = "input_grid.txt";
+    auto output_filename = "output_grid.txt";
 
-    std::string input_filename;
-    if (argc > 1)
-    {
-        input_filename = argv[1];
-    }
-    else {
-        input_filename = "input_grid.txt";
-    }
-
-    std::string output_filename = "output_grid.txt";
-    
     GridEngine ge{ output_filename };
-    ge.read_grid_from_file(input_filename);
+
+    // strategies 1 & 2
+    //auto fs = std::fstream{ output_filename, std::ios::out };
+
+    //auto g = ge.read_grid_from_file(input_filename);
+    //fs << "Starting grid:\n" << g;
+
+    //g.find_solution();
+    //fs << "Strategy 1 & 2 solution:\n" << g;
+    
+    // strategies 1 & 2 + brute force
+    ge.init_grid_from_file(input_filename);
     ge.solve_grid();
 }
